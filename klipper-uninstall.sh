9d379c69fa51c423a0020d2434e897111d0680c4
manu7irl <manu7irl@gmail.com>
2020-03-02 23:51:13 +0200

    Modified uninstall script to remove klipper & DWC

--- klipper-uninstall.sh
+++ klipper-uninstall.sh
@@ -3,22 +3,23 @@
 
 # Stop Klipper Service
 echo "#### Stopping Klipper Service.."
-sudo service klipper stop
-
-# Remove Klipper from Startup
-echo
-echo "#### Removing Klipper from Startup.."
-sudo update-rc.d -f klipper remove
+sleep 3
+#sudo service klipper stop
+sudo systemctl stop klipper.service
+sudo systemctl disable klipper.service
 
 # Remove Klipper from Services
-echo
+sleep 3
 echo "#### Removing Klipper Service.."
-sudo rm -f /etc/init.d/klipper /etc/default/klipper
-
+#sudo rm -f /etc/init.d/klipper /etc/default/klipper
+sudo rm /etc/systemd/system/klipper.service
+sleep 3
 # Notify user of method to remove Klipper source code
-echo
-echo "The Klipper system files have been removed."
-echo
-echo "The following command is typically used to remove local files:"
-echo "  rm -rf ~/klippy-env ~/klipper"
+sleep 3
+echo "The Klipper & DWC system files have been removed."
+rm -vrf ~/klipper ~/klippy-env ~/dwc2-for-klipper
+sleep 3
+echo "~/sdcard folder is not removed and still available also the printer.cfg file is not touched!"
+sleep 3
+exit
 
